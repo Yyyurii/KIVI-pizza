7bef1ce164eb5eba157ae251aef10361a253007c
Yurii <yurii.kryvko@gmail.com>
2021-09-28 22:13:31 +0300

    Create coffee-beans section

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,4 @@
 $('.pizza__controls .buttons').click(function () {
-  console.log('click');
   $(this).addClass('button-active').siblings().removeClass('button-active');
 
   let filter = $(this).attr('data-filter');
@@ -10,5 +9,17 @@ $('.pizza__controls .buttons').click(function () {
     $('.pizza .card').not('.' + filter).hide(200);
     $('.pizza .card').filter('.' + filter).show(400).css('display', 'flex');;
   }
+});
 
+$('.coffee-beans__controls .buttons').click(function () {
+  $(this).addClass('button-active').siblings().removeClass('button-active');
+
+  let filter = $(this).attr('data-filter');
+  if (filter == 'mix') {
+    $('.coffee-beans .mix').show(400);
+    $('.coffee-beans .card').not('.' + filter).hide(200);
+  } else {
+    $('.coffee-beans .card').not('.' + filter).hide(200);
+    $('.coffee-beans .card').filter('.' + filter).show(400).css('display', 'flex');;
+  }
 });
