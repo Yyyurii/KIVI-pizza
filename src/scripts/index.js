be384c9eb677d7461d2586f0849157facdcb5b99
Yurii <yurii.kryvko@gmail.com>
2021-10-02 21:08:17 +0300

    Add content to Coffee-beans section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,11 +3,11 @@ $('.pizza__controls .buttons').click(function () {
 
   let filter = $(this).attr('data-filter');
   if (filter == 'classico') {
-    $('.pizza .classico').show(400);
+    $('.pizza .classico').show(1000);
     $('.pizza .card').not('.' + filter).hide(200);
   } else {
     $('.pizza .card').not('.' + filter).hide(200);
-    $('.pizza .card').filter('.' + filter).show(400).css('display', 'flex');;
+    $('.pizza .card').filter('.' + filter).show(1000).css('display', 'flex');;
   }
 });
 
@@ -22,4 +22,40 @@ $('.coffee-beans__controls .buttons').click(function () {
     $('.coffee-beans .card').not('.' + filter).hide(200);
     $('.coffee-beans .card').filter('.' + filter).show(400).css('display', 'flex');;
   }
+});
+
+
+
+$(window).on('load scroll', function () {
+
+  if ($(window).scrollTop() > 60) {
+    $('header .header__bottom').addClass('header-active');
+  } else {
+    $('header .header__bottom').removeClass('header-active');
+  }
+
+  $('section').each(function () {
+
+    let height = $(this).height();
+    let offset = $(this).offset().top - 200;
+    let top = $(window).scrollTop();
+    let id = $(this).attr('id');
+
+    if (top >= offset && top < offset + height) {
+      $('.header__navbar ul li a').removeClass('active');
+      $('.header__navbar').find(`[href="#${id}"]`).addClass('active');
+    }
+  });
+
+  if ($(window).scrollTop() > 0) {
+    $('#scroll-up').show();
+  } else {
+    $('#scroll-up').hide();
+  }
+
+});
+
+//scroll-top
+$('.scroll-up').on('click', function () {
+  window.scrollTo(0, 0);
 });
