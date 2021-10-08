ff27d6e6c5d9da4b364afbf2d82dc0d878dff18e
Yurii <yurii.kryvko@gmail.com>
2021-10-08 21:15:47 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -55,15 +55,23 @@ $(window).on('load scroll', function () {
 
 });
 
-//scroll-top
+//scroll-up
 $('.scroll-up').on('click', function () {
   $("html, body").animate({ scrollTop: 0 }, "slow");
   return false;
 });
 
 // burger 
-$('.header__burger').click(function() {
-  $(this).toggleClass('_active'); 
+const $burgerIcon = $('.header__burger');
+$burgerIcon.click(function () {
+  $(this).toggleClass('_active');
+  $('body').toggleClass('_stop-scrolling');
+})
+$('.header__bottom a').click(() => {
+  if ($burgerIcon.hasClass('_active')) {
+    $burgerIcon.toggleClass('_active');
+    $('body').toggleClass('_stop-scrolling');
+  }
 })
 
 //loader
@@ -74,3 +82,83 @@ window.addEventListener('load', () => {
     loader.remove();
   }, 600)
 });
+
+//Add to cart
+document.addEventListener("click", documentActions);
+function documentActions(e) {
+  const targetElement = e.target;
+
+  if (targetElement.classList.contains('card__btn')) {
+    const product = targetElement.closest('.pizza__card');
+    addToCart(targetElement, product);
+    e.preventDefault();
+  }
+
+  // if (targetElement.classList.contains('cart-header__icon') || targetElement.closest('.cart-header__icon')) {
+  //   if (document.querySelector('.cart-list').children.length > 0) {
+  //     document.querySelector('.cart-header').classList.toggle('_active');
+  //   }
+  //   e.preventDefault();
+  // } else if (!targetElement.closest('.cart-header') && !targetElement.classList.contains('actions-product__button')) {
+  //   document.querySelector('.cart-header').classList.remove('_active');
+  // }
+
+  // if (targetElement.classList.contains('cart-list__delete')) {
+  //   const productId = targetElement.closest('.cart-list__item').dataset.cartPid;
+  //   updateCart(targetElement, productId, false);
+  //   e.preventDefault();
+  // }
+
+}
+function addToCart(productButton, product) {
+  if (!productButton.classList.contains('_hold')) {
+    productButton.classList.add('_hold');
+    productButton.classList.add('_fly');
+
+    const cart = document.querySelector('.actions__basket');
+    console.log(product);
+    // const productId = product;
+    const productImage = product.querySelector('.card__img');
+    console.log(productImage);
+
+    const productImageFly = productImage.cloneNode(true);
+
+    const productImageFlyWidth = productImage.offsetWidth;
+    const productImageFlyHeight = productImage.offsetHeight;
+    const productImageFlyTop = productImage.getBoundingClientRect().top;
+    const productImageFlyLeft = productImage.getBoundingClientRect().left;
+
+    productImageFly.setAttribute('class', '_flyImage _ibg');
+    productImageFly.style.cssText =
+      `
+    left: ${productImageFlyLeft}px;
+    top: ${productImageFlyTop}px;
+    width: ${productImageFlyWidth}px;
+    height: ${productImageFlyHeight}px;
+  `;
+
+    document.body.append(productImageFly);
+
+    const cartFlyLeft = cart.getBoundingClientRect().left;
+    const cartFlyTop = cart.getBoundingClientRect().top;
+
+    productImageFly.style.cssText =
+      `
+    left: ${cartFlyLeft}px;
+    top: ${cartFlyTop}px;
+    width: 0px;
+    height: 0px;
+    opacity:0;
+  `;
+
+    productImageFly.addEventListener('transitionend', function () {
+      if (productButton.classList.contains('_fly')) {
+        productImageFly.remove();
+        // updateCart(productButton, productId);
+        productButton.classList.remove('_fly');
+      }
+    });
+    
+  }
+
+}
