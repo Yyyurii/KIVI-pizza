25a01c3d79573f54c24d7a9f55ba2784cc705c8d
Yurii <yurii.kryvko@gmail.com>
2021-10-26 16:34:25 +0300

    Sender.php

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -289,3 +289,36 @@ $('#timepicker').timepicker({
   dropdown: true,
   scrollbar: true
 });
+
+jQuery(document).ready(function () {
+     
+  $(".phone").mask("+380 (99) 999-99-99"); 
+ 
+
+ jQuery('.order__btn').click( function() {
+   var form = jQuery(this).closest('form');
+   
+   if ( form.valid() ) {
+     form.css('opacity','.5');
+     var actUrl = form.attr('action');
+
+     jQuery.ajax({
+       url: actUrl,
+       type: 'post',
+       dataType: 'html',
+       data: form.serialize(),
+       success: function(data) {
+         form.html(data);
+         form.css('opacity','1');
+                 //form.find('.status').html('форма отправлена успешно');
+                 //$('#myModal').modal('show') // для бутстрапа
+       },
+       error:	 function() {
+            form.find('.status').html('серверная ошибка');
+       }
+     });
+   }
+ });
+
+
+});
