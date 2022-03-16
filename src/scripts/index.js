63b971a5ad0ae4501f35aa1ddbbd7cf07dc28665
Yyyurii <yurii.kryvko@gmail.com>
2022-03-16 22:04:32 +0200

    Функція addToCart без рендеренгу елементів

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -19,7 +19,7 @@ function renderPizza() {
       <div class="card__footer">
         <img class="card__hryvnia" src="./img/icons/hryvnia.svg" alt="hryvnia">
         <span class="card__price">${pizza.price}</span>
-        <button class="card__btn">До Кошика</button>
+        <button onclick="addToCart('${pizza.id}')" class="card__btn">До Кошика</button>
       </div>
     </div>
     `;
@@ -27,15 +27,15 @@ function renderPizza() {
 };
 renderPizza();
 
+// Масив продуктів, що зна[одяться у кошику
+const cart = [];
 
-
-
-
-
-
-
-
-
+// Додати до кошика
+function addToCart(id) {
+  const findElem = productsPizza.find(item => item.id === id);
+  cart.push({...findElem, numberOfUnits: 1});
+  console.log(cart);
+}
 
 // Вибір активної вкладки в блоці Піца
 $('.pizza__controls .buttons').click(function () {
@@ -65,71 +65,7 @@ $('.coffee-beans__controls .buttons').click(function () {
   }
 });
 
-// При скролі на навігаційному меню зверху відображає активний блок
-$(window).on('load scroll', function () {
-
-  if ($(window).scrollTop() > 60) {
-    $('header .header__bottom').addClass('header-active');
-  } else {
-    $('header .header__bottom').removeClass('header-active');
-  }
-
-  $('section').each(function () {
-
-    let height = $(this).height();
-    let offset = $(this).offset().top - 200;
-    let top = $(window).scrollTop();
-    let id = $(this).attr('id');
-
-    if (top >= offset && top < offset + height) {
-      $('.header__navbar ul li a').removeClass('active');
-      $('.header__navbar').find(`[href="#${id}"]`).addClass('active');
-    }
-  });
-
-  if ($(window).scrollTop() > 0) {
-    $('#scroll-up').show();
-  } else {
-    $('#scroll-up').hide();
-  }
-
-});
-
-//scroll-up
-$('.scroll-up').on('click', function () {
-  $("html, body").animate({ scrollTop: 0 }, "slow");
-  return false;
-});
-
-// burger 
-const $burgerIcon = $('.header__burger');
-$burgerIcon.click(function () {
-  $(this).toggleClass('_active');
-  $('body').toggleClass('_stop-scrolling');
-})
-$('.header__bottom a').click(() => {
-  if ($burgerIcon.hasClass('_active')) {
-    $burgerIcon.toggleClass('_active');
-    $('body').toggleClass('_stop-scrolling');
-  }
-})
-
-// Відображає попередній лист замовлення
-const $basket = $('.basket');
-$basket.click(function () {
-  $('.basket__list').toggleClass('_active');
-})
-
-//loader
-const loader = document.querySelector('.loader');
-window.addEventListener('load', () => {
-  loader.classList.add('_hide');
-  setTimeout(() => {
-    loader.remove();
-  }, 600)
-});
-
-//Add to cart
+//Add fly img to cart
 document.addEventListener("click", documentActions);
 
 function documentActions(e) {
@@ -142,8 +78,6 @@ function documentActions(e) {
     if (document.querySelector('.col-2__list').children.length > 0) {
       order.classList.add('_active');
       page.style.display = 'none';
-      calcPizzaCost();
-      totalPrice();
     } else {
       const emptyBasketMessage = document.querySelector('.actions p');
       emptyBasketMessage.classList.add('_active');
@@ -161,79 +95,14 @@ function documentActions(e) {
 
   if (targetElement.classList.contains('card__btn')) {
     const productId = targetElement.closest('.pizza__card').dataset.pid;;
-    addToCart(targetElement, productId);
+    addFlyImgToCart(targetElement, productId);
     e.preventDefault();
   }
-
-
-  if (targetElement.classList.contains('basket__delete-btn') || targetElement.classList.contains('basket__delete-img')) {
-    const productId = targetElement.closest('.basket__item').dataset.cartPid;
-    updateCart(targetElement, productId, false);
-    e.preventDefault();
-    checkOrderList();
-  }
-
-  if (targetElement.classList.contains('order__plus-quantity')) {
-    const parentEl = targetElement.closest(`[data-cart-pid`);
-    const quantityEl = parentEl.querySelector('.order__current-quantity');
-    quantityEl.innerHTML = ++quantityEl.innerHTML;
-
-    calcPizzaCost();
-    totalPrice();
-  }
-
-  if (targetElement.classList.contains('order__minus-quantity')) {
-    const parentEl = targetElement.closest(`[data-cart-pid`);
-    const quantityEl = parentEl.querySelector('.order__current-quantity');
-    if (quantityEl.innerHTML > 1) {
-      quantityEl.innerHTML = --quantityEl.innerHTML;
-
-      calcPizzaCost();
-      totalPrice();
-    }
-  }
-
 }
-
-//total func
-function totalPrice() {
-  const totalPrice = document.querySelector('.col-2__price-value');
-  const totalPriceElInput = document.querySelectorAll('.basket__price-total-input');
-  const currentPriceElInput = document.querySelectorAll('.basket__price-current-input');
-  let priceArr = [];
-
-  currentPriceElInput.forEach(item => {
-    priceArr.push(parseInt(item.value));
-  })
-
-  let result = priceArr.reduce(function (sum, current) {
-    return sum + current;
-  }, 0);
-
-  totalPrice.innerHTML = result + ' грн';
-  totalPriceElInput.value = result;
-}
-
-//calculate the cost of pizza
-function calcPizzaCost(parent) {
-  const productItems = document.querySelectorAll(`[data-cart-pid`);
-
-  productItems.forEach(item => {
-    const priceEl = item.querySelector('.basket__price').innerHTML;
-    const quantityEl = item.querySelector('.order__current-quantity').innerHTML;
-    const currentPriceElInput = item.querySelector('.basket__price-current-input');
-    const titleAndQuantityInput = item.querySelector('.basket__titleAndQuantityInput');
-    const titleEl = item.querySelector('.basket__title').innerHTML;
-
-    currentPriceElInput.value = parseInt(priceEl) * parseInt(quantityEl);
-    titleAndQuantityInput.value = `${titleEl} ${quantityEl}шт`;
-  });
-};
-
 
 
 //add to cart
-function addToCart(productButton, productId) {
+function addFlyImgToCart(productButton, productId) {
   if (!productButton.classList.contains('_hold')) {
     productButton.classList.add('_hold');
     productButton.classList.add('_fly');
@@ -275,54 +144,19 @@ function addToCart(productButton, productId) {
     productImageFly.addEventListener('transitionend', function () {
       if (productButton.classList.contains('_fly')) {
         productImageFly.remove();
-        updateCart(productButton, productId);
         productButton.classList.remove('_fly');
       }
     });
   }
 }
 
-// 
-const productsObj = [];
-function updateCart(productButton, productId, productAdd = true) {
-  const cart = document.querySelector('.actions');
-  const cartIcon = cart.querySelector('.basket__icon');
-  const cartQuantity = cart.querySelector('span');
-  const cartProduct = document.querySelector(`[data-cart-pid="${productId}"]`);
-
-  // Добавляем
-  if (productAdd) {
-    cartQuantity.classList.add('_active');
-
-    if (cartQuantity) {
-      cartQuantity.innerHTML = ++cartQuantity.innerHTML;
-    } else {
-      cartIcon.insertAdjacentHTML('beforeend', `<span>1</span>`);
-    }
-
-    if (!cartProduct) {
-      const orderList = document.querySelector('.col-2__list');
-      const product = document.querySelector(`[data-pid="${productId}"]`);
-      const cartProductImage = product.querySelector('.card__img');
-      const cartProductTitle = product.querySelector('.card__title').innerHTML;
-      const cartProductPrice = product.querySelector('.card__price').innerHTML;
-
-      const productObj = {
-        pizza: cartProductTitle,
-        img: cartProductImage.attributes.src.nodeValue,
-        price: cartProductPrice
-      };
-      productsObj.push(productObj);
-
-      console.log(productsObj);
-
-      const cartProductContent = `
+const cartProductContent = `
         <div class="basket__item-cont">
-          <div data-cart-pid="${productId}" class="basket__item">
-            <img class="basket__item-img" src="${cartProductImage.attributes.src.nodeValue}" alt="">
+          <div data-cart-pid="" class="basket__item">
+            <img class="basket__item-img" src="" alt="">
             <div class="basket__details">
               <input class="basket__titleAndQuantityInput" type="hidden" name="pizza_name[]" " value="">
-              <span class="basket__title">${cartProductTitle}</span>
+              <span class="basket__title"></span>
               <div class="basket__cost">
                   
                   <div class="order__quantity">
@@ -334,7 +168,7 @@ function updateCart(productButton, productId, productAdd = true) {
                   </div>
                   <div class="order__multiply">X</div>
                   <img class="basket__price-img" src="./img/icons/hryvnia-gray.svg" alt="hryvnia">
-                  <span class="basket__price">${cartProductPrice}</span>
+                  <span class="basket__price"></span>
                   <input type="hidden" name="" class="basket__price-current-input" value="" disabled>
               </div>
             </div>
@@ -345,72 +179,8 @@ function updateCart(productButton, productId, productAdd = true) {
           <hr>
         </div>
       `;
-      orderList.insertAdjacentHTML('beforeend', `${cartProductContent}`);
-      checkOrderList();
-    } else {
-      const cartProductQuantity = cartProduct.querySelector('.basket__quantity');
-      // const cartProductQuantityInput = cartProduct.querySelector('.basket__price-total-input');
-      cartProductQuantity.innerHTML = ++cartProductQuantity.innerHTML;
-      // cartProductQuantityInput.value = ++cartProductQuantityInput.value;
-    }
 
-    // После всех действий
-    productButton.classList.remove('_hold');
-  } else {
-    const cartProductQuantity = cartProduct.querySelector('.basket__quantity');
-    const cartQuantityValue = cartQuantity.innerHTML - cartProductQuantity.innerHTML;
-    cartProduct.closest('.basket__item-cont').remove();
-    calcPizzaCost();
-    totalPrice();
 
-    if (cartQuantityValue) {
-      cartQuantity.innerHTML = cartQuantityValue;
-    } else {
-      cartQuantity.innerHTML = '';
-      cartQuantity.classList.remove('_active');
-    }
-  }
-}
-
-// Вибір часу для замовлення
-$('#timepicker').timepicker({
-  timeFormat: 'HH:mm ',
-  interval: 10,
-  minTime: '09:20',
-  maxTime: '21:00',
-  defaultTime: '',
-  startTime: '09:00',
-  dynamic: false,
-  dropdown: true,
-  scrollbar: true,
-  value: this.value
-});
-
-// Відправка форми та валідація номеру телефону
-jQuery(document).ready(function () {
-  $("#phone").mask("+380 (99) 999-99-99");
-
-  jQuery('.order__btn').click(function () {
-    var form = $('form');
-
-    if (form.valid()) {
-      var actUrl = form.attr('action');
-
-      jQuery.ajax({
-        url: actUrl,
-        type: 'post',
-        dataType: 'html',
-        success: function (data) {
-          alert('Заявка відправлена');
-        },
-        error: function () {
-          form.find('.status').html('серверная ошибка');
-        }
-      });
-    }
-  });
-
-});
 
 // Перевіряємо тип замовлення
 document.querySelector('.col-1__order-type').addEventListener('click', (e) => {
@@ -446,3 +216,93 @@ function checkOrderList() {
     orderBtn.classList.remove('_disabled');
   }
 };
+
+// Відображає попередній лист замовлення
+const $basket = $('.basket');
+$basket.click(function () {
+  $('.basket__list').toggleClass('_active');
+})
+
+// burger 
+const $burgerIcon = $('.header__burger');
+$burgerIcon.click(function () {
+  $(this).toggleClass('_active');
+  $('body').toggleClass('_stop-scrolling');
+})
+$('.header__bottom a').click(() => {
+  if ($burgerIcon.hasClass('_active')) {
+    $burgerIcon.toggleClass('_active');
+    $('body').toggleClass('_stop-scrolling');
+  }
+})
+
+// Відправка форми та валідація номеру телефону
+jQuery(document).ready(function () {
+  $("#phone").mask("+380 (99) 999-99-99");
+
+  jQuery('.order__btn').click(function () {
+    var form = $('form');
+
+    if (form.valid()) {
+      var actUrl = form.attr('action');
+
+      jQuery.ajax({
+        url: actUrl,
+        type: 'post',
+        dataType: 'html',
+        success: function (data) {
+          alert('Заявка відправлена');
+        },
+        error: function () {
+          form.find('.status').html('серверная ошибка');
+        }
+      });
+    }
+  });
+
+});
+
+// При скролі на навігаційному меню зверху відображає активний блок
+$(window).on('load scroll', function () {
+
+  if ($(window).scrollTop() > 60) {
+    $('header .header__bottom').addClass('header-active');
+  } else {
+    $('header .header__bottom').removeClass('header-active');
+  }
+
+  $('section').each(function () {
+
+    let height = $(this).height();
+    let offset = $(this).offset().top - 200;
+    let top = $(window).scrollTop();
+    let id = $(this).attr('id');
+
+    if (top >= offset && top < offset + height) {
+      $('.header__navbar ul li a').removeClass('active');
+      $('.header__navbar').find(`[href="#${id}"]`).addClass('active');
+    }
+  });
+
+  if ($(window).scrollTop() > 0) {
+    $('#scroll-up').show();
+  } else {
+    $('#scroll-up').hide();
+  }
+
+});
+
+//loader
+const loader = document.querySelector('.loader');
+window.addEventListener('load', () => {
+  loader.classList.add('_hide');
+  setTimeout(() => {
+    loader.remove();
+  }, 600)
+});
+
+//scroll-up
+$('.scroll-up').on('click', function () {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
