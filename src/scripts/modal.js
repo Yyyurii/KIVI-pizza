0e7b47090ca71ee190aa1540ef358a31fb5121d8
Yyyurii <yurii.kryvko@gmail.com>
2022-03-24 21:10:48 +0200

    Рефакторинг в modal.js функції checkTypeOfDelivery

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -13,11 +13,9 @@ if (modal) {
         if (e.target === deliverBtn) {
             modal.style.display = "none";
             checkTypeOfDelivery(e);
-            console.log(e.target, 'deliver btn')
         } else if (e.target === takeMySelfBtn) {
             modal.style.display = "none";
             checkTypeOfDelivery(e);
-            console.log(e, 'takeMySelf btn')
         }
     })
 }
@@ -29,13 +27,17 @@ function checkTypeOfDelivery(e) {
     const toGoBtn = document.querySelector('.col-1__delivery-cont_toGo');
     const deliveryBlock = document.querySelector('.col-1__delivery-active');
     const deliveryBtn = document.querySelector('.col-1__delivery-cont_del');
+    const takeMySelfArr = ['col-1__delivery-cont_toGo', 'col-1__delivery-toGo-btn', 'col-1__delivery-toGo'];
+    const deliverArr = ['col-1__delivery-cont_del', 'col-1__delivery-del-btn', 'col-1__delivery-del'];
 
-    if (e.target.classList.contains('col-1__delivery-cont_toGo') || e.target.classList.contains('col-1__delivery-toGo-btn') || e.target.classList.contains('col-1__delivery-toGo')) {
+    if (takeMySelfArr.some(el => e.target.classList.contains(el))) {
         addActive(toGoBlock, toGoBtn, deliveryBlock, deliveryBtn);
+        localStorage.setItem('typeOfDelivery', 'col-1__delivery-cont_toGo');
     }
 
-    if (e.target.classList.contains('col-1__delivery-cont_del') || e.target.classList.contains('col-1__delivery-del-btn') || e.target.classList.contains('col-1__delivery-del')) {
+    if (deliverArr.some(el => e.target.classList.contains(el))) {
         addActive(deliveryBlock, deliveryBtn, toGoBlock, toGoBtn);
+        localStorage.setItem('typeOfDelivery', 'col-1__delivery-cont_del');
     }
 };
 
